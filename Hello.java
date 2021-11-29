7c002c0af12d1644cd386d0333b744af9801b54d
eomjinyoung <jinyoung.eom@gmail.com>
2021-11-29 13:35:48 +0900

    .

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!1");
-        System.out.println("Hello, world!2");
     }
 }
--- Hello.js
+++ Hello.js
@@ -1,3 +1,3 @@
 console.log("Hello, world!1");
-console.leg("Hello, world!2");
+console.log("Hello, world!2");
 console.log("Hello, world!3");
