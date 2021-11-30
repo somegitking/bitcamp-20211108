e99d04a197dec65f70683facc52f019ffcb9bec4
eomjinyoung <jinyoung.eom@gmail.com>
2021-11-30 13:21:23 +0900

    .

--- java-lang/app/src/main/java/com/eomcs/study/lang/leteral/Exam1.java
+++ java-lang/app/src/main/java/com/eomcs/study/lang/leteral/Exam1.java
@@ -1,3 +1,4 @@
+// 리터럴 : 자바에서 값을 표현한 것
 package com.eomcs.study.lang.leteral;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,9 +28,9 @@ public class Exam1 {
   }
 
   @GetMapping("/test4")
-  public float test4() {
+  public double test4() {
     // 부동소수점을 표현하는 방법
-    return 3.14f;
+    return 3.14;
   }  
 
   @GetMapping("/test5")
