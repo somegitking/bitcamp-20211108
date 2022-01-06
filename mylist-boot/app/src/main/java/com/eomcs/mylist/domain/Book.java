f1e9c24dbe325e0844e463f1dbefea4b0cc697d4
eomjinyoung <jinyoung.eom@gmail.com>
2022-01-06 15:55:14 +0900

    .

---
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/domain/Book.java
@@ -0,0 +1,13 @@
+package com.eomcs.mylist.domain;
+
+import java.sql.Date;
+
+public class Book {
+  String title;
+  String author;
+  String press;
+  int page;
+  int price;
+  Date readDate;
+  String feed;
+}
