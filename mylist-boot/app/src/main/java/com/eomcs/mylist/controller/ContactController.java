cd08cda41e82c6b5017c7b4f73873cd19ffb9f4d
eomjinyoung <jinyoung.eom@gmail.com>
2022-01-07 14:41:04 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
@@ -21,25 +21,27 @@ public class ContactController {
     StringBuilder buf = new StringBuilder();
     int c;
     while (true) {
-      c = in.read();
-      if (c == -1)
+      c = in.read(); // 파일에서 한 문자를 읽는다.
+
+      if (c == -1) // 더이상 읽을 문자가 없다면 반복문을 종료한다.
         break;
 
-      if (c == '\n') {
+      if (c == '\n') { // 만약 읽은 문자가 줄바꿈 명령이라면, 지금까지 읽은 CSV 데이터를 분석하여 Contact 객체에 담는다.
+
         String csvStr = buf.toString(); // 예) "홍길동,hong@test.com,010-1111-2222,비트캠프"
         String[] values = csvStr.split(","); // 예) ["홍길동","hong@test.com","010-1111-2222","비트캠프"]
 
-        Contact contact = new Contact();
-        contact.setName(values[0]);
+        Contact contact = new Contact(); // 파일에서 읽은 데이터를 담을 객체를 준비한다.
+        contact.setName(values[0]); // 배열에 들어 있는 각 항목을 객체의 필드에 저장한다.
         contact.setEmail(values[1]);
         contact.setTel(values[2]);
         contact.setCompany(values[3]);
 
-        contactList.add(contact);
+        contactList.add(contact); // 데이터를 담은 객체를 목록에 추가한다.
 
-        buf.setLength(0);
+        buf.setLength(0); // 다음 데이터를 읽기 위해 버퍼를 초기화시킨다.
 
-      } else {
+      } else { // 문자를 읽을 때 마다 버퍼이 임시 보관한다.
         buf.append((char) c);
       }
     }
