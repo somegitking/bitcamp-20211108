a1f484c3097ad3a3d514204f9ac96cdb3eeb772b
eomjinyoung <jinyoung.eom@gmail.com>
2022-01-19 14:15:05 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/ContactController.java
@@ -1,9 +1,11 @@
 package com.eomcs.mylist.controller;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.eomcs.mylist.domain.Contact;
@@ -18,11 +20,21 @@ public class ContactController {
     contactList = new ArrayList();
     System.out.println("ContactController() 호출됨!");
 
-    BufferedReader in = new BufferedReader(new FileReader("contacts.csv"));
+    DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream("contacts.data")));
 
-    String line;
-    while ((line = in.readLine()) != null) { // readLine()이 null을 리턴한다면 파일을 다 읽었다는 것이다.
-      contactList.add(Contact.valueOf(line)); // 파일에서 읽은 한 줄의 CSV 데이터로 객체를 만든 후 목록에 등록한다.
+    while (true) {
+      try {
+        Contact contact = new Contact();
+        contact.setName(in.readUTF());
+        contact.setEmail(in.readUTF());
+        contact.setTel(in.readUTF());
+        contact.setCompany(in.readUTF());
+
+        contactList.add(contact);
+
+      } catch (Exception e) {
+        break;
+      }
     }
 
     in.close();
@@ -74,12 +86,15 @@ public class ContactController {
   @RequestMapping("/contact/save")
   public Object save() throws Exception {
 
-    PrintWriter out = new PrintWriter(new FileWriter("contacts.csv")); // 따로 경로를 지정하지 않으면 파일은 프로젝트 폴더에 생성된다.
+    DataOutputStream out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("contacts.data")));
 
     Object[] arr = contactList.toArray();
     for (Object obj : arr) {
       Contact contact = (Contact) obj;
-      out.println(contact.toCsvString());
+      out.writeUTF(contact.getName());
+      out.writeUTF(contact.getEmail());
+      out.writeUTF(contact.getTel());
+      out.writeUTF(contact.getCompany());
     }
 
     out.close();
