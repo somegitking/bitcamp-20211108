35afd1e0800d985c4be3ac49534348b82677dad5
eomjinyoung <jinyoung.eom@gmail.com>
2022-01-25 16:50:35 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/TodoController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/TodoController.java
@@ -1,89 +1,48 @@
 package com.eomcs.mylist.controller;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.eomcs.mylist.dao.TodoDao;
 import com.eomcs.mylist.domain.Todo;
-import com.eomcs.util.ArrayList;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController 
 public class TodoController {
 
-  ArrayList todoList = new ArrayList();
-
-  public TodoController() throws Exception {
-    System.out.println("TodoController() 호출됨!");
-
-    try {
-      BufferedReader in = new BufferedReader(new FileReader("todos.json"));
-
-      ObjectMapper mapper = new ObjectMapper();
-      todoList = new ArrayList(mapper.readValue(in.readLine(), Todo[].class));
-
-      in.close();
-    } catch (Exception e) {
-      System.out.println("해야 할 일 데이터를 로딩하는 중에 오류 발생!");
-    }
-  }
+  @Autowired
+  TodoDao todoDao;
 
   @RequestMapping("/todo/list")
   public Object list() {
-    return todoList.toArray(); 
+    return todoDao.findAll(); 
   }
 
   @RequestMapping("/todo/add")
-  public Object add(Todo todo) {
-    todoList.add(todo);
-    return todoList.size();
+  public Object add(Todo todo) throws Exception {
+    todoDao.insert(todo);
+    return todoDao.countAll();
   }
 
   @RequestMapping("/todo/update")
-  public Object update(int index, Todo todo) {
-    if (index < 0 || index >= todoList.size()) {
+  public Object update(int index, Todo todo) throws Exception {
+    Todo old = todoDao.findByNo(index);
+    if (old == null) {
       return 0;
     }
-
-    Todo old = (Todo) todoList.get(index);
     todo.setDone(old.isDone()); // 기존의 체크 정보를 그대로 가져가야 한다.
-
-    return todoList.set(index, todo) == null ? 0 : 1;
+    return todoDao.update(index, todo);
   }
 
   @RequestMapping("/todo/check")
-  public Object check(int index, boolean done) {
-    if (index < 0 || index >= todoList.size()) {
-      return 0;  // 인덱스가 무효해서 설정하지 못했다.
-    }
-
-    ((Todo) todoList.get(index)).setDone(done);
-    return 1; // 해당 항목의 상태를 변경했다.
+  public Object check(int index, boolean done) throws Exception {
+    return todoDao.updateDone(index, done);
   }
 
   @RequestMapping("/todo/delete")
-  public Object delete(int index) {
-    if (index < 0 || index >= todoList.size()) {
-      return 0;
-    }
-
-    todoList.remove(index);
-    return 1;
+  public Object delete(int index) throws Exception {
+    return todoDao.delete(index);
   }
 
-  @RequestMapping("/todo/save")
-  public Object save() throws Exception {
-    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("todos.json")));
-
-    ObjectMapper mapper = new ObjectMapper();
-    out.println(mapper.writeValueAsString(todoList.toArray()));
-
-    out.close();
-    return todoList.size();
-  }
 }
 
 
