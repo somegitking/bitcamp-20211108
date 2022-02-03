34e42588184a233b5a9f6886c55c70387cf215fa
eomjinyoung <jinyoung.eom@gmail.com>
2022-02-03 11:36:18 +0900

    .

---
+++ project-app1/app-server/src/main/java/com/eomcs/app1/CalculateController.java
@@ -0,0 +1,31 @@
+package com.eomcs.app1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculateController {
+
+  @RequestMapping("/calc")
+  public String calc(String op, double a, double b) {
+    String response = null;
+
+    switch (op) {
+      case "+": 
+        response = String.format("강사: %.1f + %.1f = %.2f", a, b, (a + b));
+        break;
+      case "-": 
+        response = String.format("강사: %.1f - %.1f = %.2f", a, b, (a - b));
+        break;
+      case "*": 
+        response = String.format("강사: %.1f * %.1f = %.2f", a, b, (a * b));
+        break;  
+      case "/": 
+        response = String.format("강사: %.1f / %.1f = %.2f", a, b, (a / b));
+        break;
+      default: 
+        response = "강사: 지원하지 않는 연산자입니다.";
+    }
+    return response;
+  }
+}
