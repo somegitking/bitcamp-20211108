83f130c96d7e897aafe8a5a09ec61cec77cd58e1
eomjinyoung <jinyoung.eom@gmail.com>
2022-02-16 11:11:41 +0900

    .

--- project-app2/app/src/main/java/com/eomcs/app2/App.java
+++ project-app2/app/src/main/java/com/eomcs/app2/App.java
@@ -3,20 +3,20 @@ package com.eomcs.app2;
 import java.util.Scanner;
 
 public class App {
+
+  Scanner keyScan = new Scanner(System.in);
+
   public static void main(String[] args) {
-    Scanner keyScan = new Scanner(System.in);
+    new App().service();
+  }
+
+  public void service() {
 
     while (true) {
-      System.out.println("메뉴:");
-      System.out.println("1. 등록");
-      System.out.println("2. 목록");
-      System.out.println("3. 상세");
-      System.out.println("4. 변경");
-      System.out.println("5. 삭제");
-      System.out.print("명령> ");
-      String input = keyScan.nextLine();
+      printMenu();
+      String input = prompt();
 
-      if (input.equals("quit") || input.equals("exit")) {
+      if (checkQuit(input)) {
         break;
       }
 
@@ -39,4 +39,51 @@ public class App {
     System.out.println("종료!");
     keyScan.close();
   }
+
+  private void printMenu() {
+    System.out.println("메뉴:");
+    System.out.println("1. 등록");
+    System.out.println("2. 목록");
+    System.out.println("3. 상세");
+    System.out.println("4. 변경");
+    System.out.println("5. 삭제");
+  }
+
+  private String prompt() {
+    System.out.print("명령> ");
+    return keyScan.nextLine();
+  }
+
+  private boolean checkQuit(String input) {
+    return input.equals("quit") || input.equals("exit");
+  }
+
+  private void createScore() {
+
+  }
+
+  private void listScore() {
+
+  }
+
+  private void detailScore() {
+
+  }
+
+  private void updateScore() {
+
+  }
+
+  private void deleteScore() {
+
+  }
 }
+
+
+
+
+
+
+
+
+
